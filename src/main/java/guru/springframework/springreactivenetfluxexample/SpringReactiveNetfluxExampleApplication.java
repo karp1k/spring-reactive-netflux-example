ebcc73ac23203abc7e577d08daecd23942189fb6
Tom <desktop-vm-ubuntu>
2020-06-05 17:36:07 +0300

    Init project

---
+++ src/main/java/guru/springframework/springreactivenetfluxexample/SpringReactiveNetfluxExampleApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springreactivenetfluxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveNetfluxExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveNetfluxExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/springreactivenetfluxexample/SpringReactiveNetfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springreactivenetfluxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveNetfluxExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
