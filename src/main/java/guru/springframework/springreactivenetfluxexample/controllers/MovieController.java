c82942c5090340b19c41f9a5fdd32c8e2eefbb13
Tom <desktop-vm-ubuntu>
2020-06-05 18:23:23 +0300

    add producing mediatype

--- src/main/java/guru/springframework/springreactivenetfluxexample/controllers/MovieController.java
+++ src/main/java/guru/springframework/springreactivenetfluxexample/controllers/MovieController.java
@@ -3,6 +3,7 @@ package guru.springframework.springreactivenetfluxexample.controllers;
 import guru.springframework.springreactivenetfluxexample.domain.Movie;
 import guru.springframework.springreactivenetfluxexample.events.MovieEvent;
 import guru.springframework.springreactivenetfluxexample.services.MovieService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,8 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/{id}/event")
+    // by default is json, but in this case method movieService.event() return stream of data
+    @GetMapping(value = "/{id}/event", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     Flux<MovieEvent> streamMovieEvent(@PathVariable String id) {
         return movieService.events(id);
     }
